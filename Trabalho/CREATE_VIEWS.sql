797d22da4b6bb829268ce3c2e89d21d0e2e9c27e
Dude29 <joaopc29@hotmail.com>
2016-11-29 20:07:13 +0000

    Added forgotten existence checks to AluguerView

--- Trabalho/CREATE_VIEWS.sql
+++ Trabalho/CREATE_VIEWS.sql
@@ -11,6 +11,10 @@ SELECT cId, nif, nome, morada FROM Cliente WHERE valido = 1
 
 GO
 
+IF OBJECT_ID('dbo.AluguerView') IS NOT NULL
+	DROP VIEW dbo.AluguerView
+GO
+
 CREATE VIEW dbo.AluguerView AS -- view para mostrar apenas os alugueres a decorrer
 SELECT serial, eqId, empregado, cliente, data_inicio
 FROM dbo.Aluguer
