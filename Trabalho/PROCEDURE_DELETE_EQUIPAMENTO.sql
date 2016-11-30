9e540825674d5996f103c96f76a2173a3f37925e
Dude29 <joaopc29@hotmail.com>
2016-11-30 00:30:20 +0000

    Changed equipment deletion procedure to verify if equipments exist before deleting

--- Trabalho/PROCEDURE_DELETE_EQUIPAMENTO.sql
+++ Trabalho/PROCEDURE_DELETE_EQUIPAMENTO.sql
@@ -5,4 +5,22 @@ IF OBJECT_ID('dbo.RemoverEquipamento') IS NOT NULL
 GO
 CREATE PROCEDURE dbo.RemoverEquipamento @id INT
 AS
+	BEGIN TRAN
+	DECLARE @now DATETIME = GETDATE()
+
+	IF EXISTS(
+		SELECT eqId
+		FROM dbo.AluguerView
+		INNER JOIN dbo.AluguerDataFim ON(serial = serial_adf)
+		WHERE(eqId = @id AND data_fim > @now)
+	) 
+	BEGIN
+		-- nao sei bem se isto e o mais correto a fazer, mas como nao sabia como proceder fiz isto
+		DECLARE @msg VARCHAR(MAX) = CONCAT('O equipamento com eid=', @id, ' está em uso'); 
+		THROW 50129, @msg, 1 
+	END
+
 	DELETE FROM Equipamento WHERE eqId = @id
+
+	COMMIT
+GO
