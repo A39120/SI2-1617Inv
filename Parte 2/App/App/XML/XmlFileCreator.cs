b83c4fc15d8060aec47cf4954b1566a17f101403
notLopes <lariachi@gmail.com>
2017-01-08 16:16:50 +0000

    Created AluguerSchema.cs

---
+++ Parte 2/App/App/XML/XmlFileCreator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+using System.Xml;
+
+namespace App.XML
+{
+    class XmlFileCreator
+    {
+        private XmlSampleGenerator xsg;
+        public void CreateXmlFileFromSchema(XmlSchema schema, XmlQualifiedName rootElem)
+        {
+            xsg = XmlSampleGenerator(schema, rootElem);
+        }
+
+
+    }
+}
