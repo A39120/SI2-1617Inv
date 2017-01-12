7fb1047954dd890dd27d7c0862dbcab91d6e3301
TiagoSSantos <wolf_93_@hotmail.com>
2017-01-12 20:59:27 +0000

    Update to UnitTest1

--- Parte 2/App/UnitTests/UnitTest1.cs
+++ Parte 2/App/UnitTests/UnitTest1.cs
@@ -14,33 +14,42 @@ namespace UnitTests {
         [TestMethod]
         public void CompareADO_VS_EF() {
             Stopwatch sw = new Stopwatch();
-            int MAX_RUNS = 20;
+            int MAX_RUNS = 50;
 
             // Test EF speed for EquipamentosSemAlugueresNaUltimaSemana
+            long EFBestTime = int.MaxValue;
             sw.Start();
-            for (int i = 0; i < MAX_RUNS; i++ )
+            for (int i = 0; i < MAX_RUNS; i++) {
+                Stopwatch sw2 = new Stopwatch();
+                sw2.Start();
                 using (EfCommand cmd = new EfCommand()) {
                     cmd.EquipamentosSemAlugueresNaUltimaSemana();
                 }
+                sw2.Stop();
+                EFBestTime = (sw2.ElapsedTicks < EFBestTime) ? sw2.ElapsedTicks : EFBestTime;
+            }
             sw.Stop();
             long EFtime = sw.ElapsedMilliseconds;
 
             // Test ADO.NET speed for EquipamentosSemAlugueresNaUltimaSemana
             sw.Reset();
             sw.Start();
-            for (int i = 0; i < MAX_RUNS; i++)
-                using (SqlConnection con = new SqlConnection()) {
-                    con.ConnectionString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
-                    using (AdoCommand cmd = new AdoCommand()) {
-                        cmd.EquipamentosSemAlugueresNaUltimaSemana();
-                    }
+            long ADOdotNETBestTime = int.MaxValue;
+            for (int i = 0; i < MAX_RUNS; i++) {
+                Stopwatch sw2 = new Stopwatch();
+                sw2.Start();
+                using (AdoCommand cmd = new AdoCommand()) {
+                    cmd.EquipamentosSemAlugueresNaUltimaSemana();
                 }
+                sw2.Stop();
+                ADOdotNETBestTime = (sw2.ElapsedTicks < ADOdotNETBestTime) ? sw2.ElapsedTicks : ADOdotNETBestTime;
+            }
             sw.Stop();
             long ADOdotNETtime = sw.ElapsedMilliseconds;
 
             Console.WriteLine(
-                "EF: " + EFtime + "ms" +
-                "\nADO.NET: " + ADOdotNETtime + "ms"
+                "EF -> Total time: " + EFtime + "ms Best time: " + EFBestTime + "ticks" +
+                "\nADO.NET -> Total time:" + ADOdotNETtime + "ms Best time: " + ADOdotNETBestTime + "ticks"
             );
         }
     }
